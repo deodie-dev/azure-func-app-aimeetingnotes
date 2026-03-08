a5fcb91d6ad7e52690f733974b2041a5aaafab58
Deodie <deodie.dev@gmail.com>
2026-03-08 20:47:11 +0800

    Update core modules and add hidden clickup custom field IDs

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,5 @@
 import os
-from src.core.folder_IDs import BA_FOLDER_LIST
+from src.core.clickup_ids import *
 
 class Config:
 
@@ -16,18 +16,30 @@ class Config:
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     OPENAI_URL = os.getenv("OPENAI_URL")
 
-    #Azure SQL Database credentials
+    # Azure SQL Database credentials
     SQL_DATABASE = os.getenv("SQL_DATABASE")
     SQL_DRIVER = os.getenv("SQL_DRIVER")
     SQL_PASSWORD = os.getenv("SQL_PASSWORD")
     SQL_SERVER = os.getenv("SQL_SERVER")
     SQL_USERNAME = os.getenv("SQL_USERNAME")
 
-    #Clickup folder IDs
+    # ClickUp folder and list IDs
     DIAGNOSTIC_ID = os.getenv("DIAGNOSTIC_ID")
     RETAINER_ID = os.getenv("RETAINER_ID")
     CALENDAR_EVENTS_LIST_ID = os.getenv("CALENDAR_EVENTS_LIST_ID")
     OTHERS_LIST_ID = os.getenv("OTHERS_LIST_ID")
     BA_LIST = BA_FOLDER_LIST
 
+    # ClickUp custom field IDs
+    CUSTOM_FIELD_ID_BusinessAdviser = CUSTOM_FIELD_ID_BusinessAdviser
+    CUSTOM_FIELD_ID_Organizer = CUSTOM_FIELD_ID_Organizer
+    CUSTOM_FIELD_ID_MeetingCancelled = CUSTOM_FIELD_ID_MeetingCancelled
+    CUSTOM_FIELD_ID_StartTime = CUSTOM_FIELD_ID_StartTime
+    CUSTOM_FIELD_ID_Duration = CUSTOM_FIELD_ID_Duration
+    CUSTOM_FIELD_ID_Categories = CUSTOM_FIELD_ID_Categories
+    CUSTOM_FIELD_ID_Attendees = CUSTOM_FIELD_ID_Attendees
+    CUSTOM_FIELD_ID_TranscriptFound = CUSTOM_FIELD_ID_TranscriptFound
+    CUSTOM_FIELD_ID_AIAPIDone = CUSTOM_FIELD_ID_AIAPIDone
+    CUSTOM_FIELD_ID_SummarizedTranscript = CUSTOM_FIELD_ID_SummarizedTranscript
+    CUSTOM_FIELD_ID_ClickUpAPIDone = CUSTOM_FIELD_ID_ClickUpAPIDone
 
